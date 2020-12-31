2bcc1dedaaeff5f918e8fb1a7768d21d5b24b920
adrian.chirita <adrian.chirita@SVROLP01972.ro.softvision.global>
2020-12-31 13:01:55 +0200

    django startup script for ansible + logging

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+
+
+python3 manage.py makemigrations >> ~/log
+python3 manage.py migrate >> ~/log
+python3 manage.py runserver 0.0.0.0:8000 >> ~/log
