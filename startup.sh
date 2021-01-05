b12cd029371074e8158c2ddc2eccca109eb13551
adrian.chirita <adrian.chirita@SVROLP01972.ro.softvision.global>
2021-01-05 14:57:40 +0200

    relative path fix for script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
 python3 manage.py makemigrations >> ~/log
 python3 manage.py migrate >> ~/log
 python3 manage.py runserver 0.0.0.0:8000 >> ~/log
